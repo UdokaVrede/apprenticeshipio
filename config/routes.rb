270188e4276a81b88e83e63fd3e066be05634d26
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2020-01-24 19:05:28 -0300

    Refactored card component, added routes, created partner model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home', as: :home
+  get 'about_us', to: 'pages#about_us', as: :about_us
+  get 'our_data', to: 'pages#our_data', as: :our_data
+  get 'hall_of_fame', to: 'pages#hall_of_fame', as: :hall_of_fame
+  get 'resources', to: 'pages#resources', as: :resources
+  get 'apply_now', to: 'pages#apply_now', as: :apply_now
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
