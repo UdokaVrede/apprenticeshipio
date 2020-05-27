a5545d3abaf56f66c99a7737acdf912953ae910e
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2020-05-27 11:07:03 -0300

    Updated Resources links, added team member cards, began Airtable API job

---
+++ app/jobs/updateairtable_job.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+
+class UpdateAirtableJob < ApplicationJob
+  queue_as :default
+
+  def fetch_airtable_api(table)
+    airtable_api = "https://api.airtable.com/v0/app73alvcuASBVNl1/#{table}"
+
+    table_json = open(airtable_api).read
+    table_info = JSON.parse(table_json)
+    tables_array =
+    tables_array.each do |table|
+      # TODO decide what to do
+  end
+end
+
+  def perform
+    tables_array = ["apprentices", "companies", "partners", "schools"]
+
+    tables_array.each do |table|
+      fetch_airtable_api(table)
+    end
+  end
+end
+
