b2fa67cdc004ae972a1fb17d7d635253f63f99ad
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2020-08-17 09:51:59 -0300

    Updated loop SVGs and blog posts on Resources page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 // = link apprenticeshipio-circle-cropped.png
+// = link LargeLoop.svg
+// = link MediumLoop.svg
+// = link SmallLoop.svg
+// = link TinyLoop.svg
